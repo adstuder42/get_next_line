4a9ca16e317c98f81f895a6b8d11a39aede233b7
Adrien <officialstunder@gmail.com>
2019-09-07 15:49:54 +0200

    maj gnl

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adstuder <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/02 21:12:50 by adstuder          #+#    #+#             */
+/*   Updated: 2019/02/28 15:56:41 by adstuder         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1
+
+# include "libft/libft.h"
+# include <stdio.h>
+
+int					get_next_line(const int fd, char **line);
+
+#endif
---
+++ libft/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adstuder <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/23 21:44:42 by adstuder          #+#    #+#             */
+/*   Updated: 2019/02/27 15:53:42 by adstuder         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+char				*ft_strdup(const char *s1);
+size_t				ft_strlen(const char *s);
+
+#endif
